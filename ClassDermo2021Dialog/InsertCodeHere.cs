7cbf4277952375136f149a2ea8a2ed3a704917df
Peter Levinsky <pele@easj.dk>
2021-10-15 10:40:48 +0200

    Add project files.

---
+++ ClassDermo2021Dialog/InsertCodeHere.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassDermo2021Dialog
+{
+    public class InsertCodeHere
+    {
+        public InsertCodeHere()
+        {
+        }
+
+        public void MyCode()
+        {
+            bool fortsæt = true;
+            while (fortsæt)
+            {
+                Console.WriteLine("Hvor vil du hen ?");
+                Console.WriteLine("V: venstre");
+                Console.WriteLine("H: højre");
+                Console.WriteLine("O: op");
+                Console.WriteLine("N: ned");
+                Console.WriteLine("stop: stopper program");
+
+                string input = Console.ReadLine();
+
+                switch (input.ToUpper())
+                {
+                    case "V": Console.WriteLine("Går mod vest");
+                        break;
+                    case "H":
+                        Console.WriteLine("Går mod øst");
+                        break;
+                    case "O":
+                        Console.WriteLine("Går mod nord");
+                        break;
+                    case "N":
+                        Console.WriteLine("Går mod syd");
+                        break;
+                    case "STOP":
+                        Console.WriteLine("Vi stopper programmet");
+                        fortsæt = false;
+                        break;
+                    default:
+                        Console.WriteLine("Du bør bruge V,H,O,N eller stop !");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ ClassDermo2021Dialog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassDermo2021Dialog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertCodeHere code = new InsertCodeHere();
+            code.MyCode();
+
+            Console.ReadLine();
+        }
+    }
+}
